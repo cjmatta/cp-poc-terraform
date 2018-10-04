6d3c2d57095c6b47ba24e5349dadc5c48dcc6ab5
Mic Hussey <mic@confluent.io>
2018-10-04 11:28:55 +0200

    Added flag to switch between public and private DNS

--- create_ansible_inventory.py
+++ create_ansible_inventory.py
@@ -3,6 +3,7 @@
 # output of `https://github.com/cjmatta/cp-poc-terraform
 # Usage: terraform output -json` | ./create_ansible_inventory.py
 import json
+import argparse
 from jinja2 import Template
 import sys
 
@@ -14,23 +15,23 @@ template = Template("""all:
     security_mode: sasl_ssl
 preflight:
   hosts:
-{%- for broker in broker_public_dns.value %}
+{%- for broker in broker_dns.value %}
     {{broker}}:
 {%- endfor %}
-{%- for worker in worker_public_dns.value %}
+{%- for worker in worker_dns.value %}
     {{worker}}:
 {%- endfor %}
 ssl_CA:
   hosts:
-    {{broker_public_dns.value[0]}}:
+    {{broker_dns.value[0]}}:
 zookeeper:
   hosts:
-{%- for broker in broker_public_dns.value %}
+{%- for broker in broker_dns.value %}
     {{broker}}:
 {%- endfor %}
 broker:
   hosts:
-{%- for broker in broker_public_dns.value %}
+{%- for broker in broker_dns.value %}
     {{broker}}:
       kafka:
         broker:
@@ -38,38 +39,38 @@ broker:
 {%- endfor %}
 schema-registry:
   hosts:
-{%- for worker in worker_public_dns.value %}
+{%- for worker in worker_dns.value %}
     {{worker}}:
 {%- endfor %}
 control-center:
   hosts:
-    {{worker_public_dns.value[0]}}:
+    {{worker_dns.value[0]}}:
       confluent:
         control:
           center:
             config:
-              confluent.controlcenter.connect.cluster: {% for worker in worker_public_dns.value -%}
+              confluent.controlcenter.connect.cluster: {% for worker in worker_dns.value -%}
                 {{worker}}:8083
                 {%- if not loop.last %},{% endif %}
                 {%- endfor %}
 connect-distributed:
   hosts:
-{%- for worker in worker_public_dns.value %}
+{%- for worker in worker_dns.value %}
     {{worker}}:
 {%- endfor %}
 kafka-rest:
   hosts:
-{%- for worker in worker_public_dns.value %}
+{%- for worker in worker_dns.value %}
     {{worker}}:
 {%- endfor %}
 ksql:
   hosts:
-{%- for worker in worker_public_dns.value %}
+{%- for worker in worker_dns.value %}
     {{worker}}:
 {%- endfor %}
 tools:
   hosts:
-    {{worker_public_dns.value[0]}}:""")
+    {{worker_dns.value[0]}}:""")
 
 def read_stdin_json():
     try:
@@ -80,4 +81,19 @@ def read_stdin_json():
         sys.exit(1)
 
 input = read_stdin_json()
-print(template.render(input))
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--public", action="store_true",
+	help="Use public DNS from terraform, default is private")
+args = ap.parse_args()
+
+if args.public:
+    dns_type = 'public'
+else:
+    dns_type = 'private'
+template_params = {
+    "worker_dns": input['worker_'+dns_type+'_dns'], 
+    "broker_dns": input['broker_'+dns_type+'_dns']
+}
+print(template.render(template_params))
